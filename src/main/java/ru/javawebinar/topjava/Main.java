5ee71d7f293fd19483031c8f8fc0e8009ac89b7f
Konstantin Ostrovsky <miontq@gmail.com>
2017-07-15 14:58:15 +0300

    1-6-logging

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -6,6 +6,8 @@ package ru.javawebinar.topjava;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.format("Hello Topjava Enterprise!");
+
+        System.out.format("Hello Topjava Enterprise!\n");
+        System.out.println(System.getenv("TOPJAVA_ROOT"));
     }
 }
